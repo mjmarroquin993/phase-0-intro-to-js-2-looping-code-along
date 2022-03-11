e516fcbdfd970a1dc0d8883e42f00c3977b8dd28
Josh Marroquin <mj.marroquin993@gmail.com>
2022-03-11 13:34:38 -0800

    commit tree 0e62784871fd27d9bc9b0d9f896aa21a77ea4b7b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const writeCards = (array, event) => {
+    const cards = []
+    for (let i = 0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+const countDown = number => {
+   while (number >= 0) {
+       console.log(number)
+       number--
+   }
+}
